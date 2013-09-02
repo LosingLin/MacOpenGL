fdadf4fd671c2bb6a6bf1fa6f961029b4c173c6d
LosingLin <764837522@qq.com>
2013-09-02 11:08:25 +0800

    Initial Commit

---
+++ MacOpenGL/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  MacOpenGL
+//
+//  Created by qiusnay on 13-9-2.
+//  Copyright (c) 2013年 qiusnay. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
